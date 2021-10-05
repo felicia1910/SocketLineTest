9b1796727b0d42b48695375fbd76751c3b552fd0
Felicia <flinder410010@gmail.com>
2021-10-05 14:12:05 +0800

    try

--- index.js
+++ index.js
@@ -3,14 +3,16 @@ const linebot = require('linebot');
 const express = require('express');
 const BodyParser = require('body-parser');
 const cors = require('cors');
+//引入自訂義
+const bot = require('./line/index');
 // Line Channel info
-const bot = linebot({
-    channelId: '1656330672',//process.env.channelId,
-    channelSecret: '2036133d57ee3e0f6f318423c0000ee9',
-    channelAccessToken: 'htgAj7q1TMGeyxIzhHw7ExEXT00eg5UuLzHzLiFz+OeeN7U/2+IZtX2i9YFHQcLszMd+Cvmt3r8/1wZTmKznGz5vC54lQU/VT3msf/HZt8dG5Ny3L3dKkR+qE/rsbPGmiMpXB6SpA9sRRCQVVPhFCAdB04t89/1O/w1cDnyilFU=',
-});
+// const bot = linebot({
+//     channelId: '1656330672',//process.env.channelId,
+//     channelSecret: '2036133d57ee3e0f6f318423c0000ee9',
+//     channelAccessToken: 'htgAj7q1TMGeyxIzhHw7ExEXT00eg5UuLzHzLiFz+OeeN7U/2+IZtX2i9YFHQcLszMd+Cvmt3r8/1wZTmKznGz5vC54lQU/VT3msf/HZt8dG5Ny3L3dKkR+qE/rsbPGmiMpXB6SpA9sRRCQVVPhFCAdB04t89/1O/w1cDnyilFU=',
+// });
 
-const linebotParser = bot.parser();
+const linebotParser = bot.bot.parser();
 const app = express();
 // for line webhook usage
 app.post('/linewebhook', linebotParser);
@@ -21,7 +23,7 @@ app.use(BodyParser.json());
 const port = process.env.PORT || 4000;
 // a http endpoint for trigger broadcast
 app.post('/broadcast', (req, res) => {
-  bot.broadcast(req.body.message).then(() => {
+    bot.bot.broadcast(req.body.message).then(() => {
     res.send('broadcast ok');
   }).catch(function (error) {
     res.send('broadcast fail');
@@ -31,13 +33,13 @@ app.post('/broadcast', (req, res) => {
 app.listen(port);
 
 // echo user message
-bot.on('message', function (event) {
-  // get user message from `event.message.text`
-  // reply same message
-  var replyMsg = `${event.message.text}`;
-  event.reply(replyMsg).then(function (data) {
-    console.log('ok')
-  }).catch(function (error) {
-    console.error(error)
-  });
-});
+// bot.on('message', function (event) {
+//   // get user message from `event.message.text`
+//   // reply same message
+//   var replyMsg = `${event.message.text}`;
+//   event.reply(replyMsg).then(function (data) {
+//     console.log('ok')
+//   }).catch(function (error) {
+//     console.error(error)
+//   });
+// });
