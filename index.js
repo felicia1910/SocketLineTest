b994fbbbaf6d84d2f833983b6386e6b7edc5637b
Felicia <flinder410010@gmail.com>
2021-10-05 14:39:04 +0800

    復原..

--- index.js
+++ index.js
@@ -28,29 +28,29 @@ app.listen(port);
 
 const server = http.createServer(app);
 //>V3後會有cors的問題
-const io = socketio(server, {
-    cors: {
-        origin: '*',
-    }
-});
+// const io = socketio(server, {
+//     cors: {
+//         origin: '*',
+//     }
+// });
 //用 socket 方式取得
-io.on('connection', function (socket) {
-    console.log('user connected')
-    socket.emit("allMessage", messages)
-    // let test=bot.getBot();
-    // console.log('test??',test)
-    socket.on("sendMessage", function (mes) {
-        console.log('??', mes)
-        //找到有沒有這個人的聊天紀錄
-        // let find = all.map(e => { return e.name }).indexOf(mes.name);
-        // console.log('find', find);
-        // if (find !== -1) {
-        //     all[find].mes.push(mes)
-        // } else {
-        //     all.push({ name: mes.name, mes: [mes] })
-        // }
-        // console.log('all', all)
-        // io.emit("newMessage", all)
-    })
+// io.on('connection', function (socket) {
+//     console.log('user connected')
+//     socket.emit("allMessage", messages)
+//     // let test=bot.getBot();
+//     // console.log('test??',test)
+//     socket.on("sendMessage", function (mes) {
+//         console.log('??', mes)
+//         //找到有沒有這個人的聊天紀錄
+//         // let find = all.map(e => { return e.name }).indexOf(mes.name);
+//         // console.log('find', find);
+//         // if (find !== -1) {
+//         //     all[find].mes.push(mes)
+//         // } else {
+//         //     all.push({ name: mes.name, mes: [mes] })
+//         // }
+//         // console.log('all', all)
+//         // io.emit("newMessage", all)
+//     })
 
-})
+// })
