22a8ef449d753d37e86fbe8f4cc1567e0322b754
Felicia <flinder410010@gmail.com>
2021-10-18 16:19:56 +0800

    可以接貼圖根圖片，圖片怎麼存進api要再想想..

--- line/index.js
+++ line/index.js
@@ -1,6 +1,11 @@
 const linebot = require('linebot');
 const line = require('@line/bot-sdk');
 const { io } = require("socket.io-client");
+//這邊要注意fetch版本太高會有問題
+const fs = require('fs');
+const cp = require('child_process');
+const path = require('path');
+const fetch = require('node-fetch');
 require('dotenv').config();
 //引入自訂義
 const lineChannel = require('./setting');
@@ -14,8 +19,8 @@ const client = new line.Client({
 
 let socket = null;
 const socketClient = () => {
-  socket = io("https://socketio-server-testing.herokuapp.com/");
-  //socket = io("http://localhost:4010/");
+  //socket = io("https://socketio-server-testing.herokuapp.com/");
+  socket = io("http://localhost:4010/");
 };
 
 const sendMessage = (mes) => {
@@ -30,34 +35,70 @@ const getBot = () => {
   bot.on('message', async event => {
     console.log('event.message.text', event);
 
-    // let mes = {
-    //   type: 'text',
-    //   text: 'hello,world'
-    // }
+    let doObj = (event, ee) => {
+      return {
+        type: event.message.type,
+        textId: event.message.id,
+        name: ee.userName,
+        userId: event.source.userId,
+        pic: ee.pic,
+        timestamp: event.timestamp,
+        time: new Date(event.timestamp),
+        st: ee.st,
+        which: 1,
+        user: true
+      }
+    }
     if (event.message.type === 'text') {
       catchUser(event.source.userId).then(ee => {
         if (ee) {
+          let getObj = doObj(event, ee);
           sendMessage({
-            type:event.message.type,
-            textId:event.message.id,
+            ...getObj,
             message: event.message.text,
-            name: ee.userName,
-            userId: event.source.userId,
-            pic: ee.pic,
-            timestamp: event.timestamp,
-            time:new Date(event.timestamp),
-            st:ee.st,
-            which:1,
-            user:true
           });
           event.reply(event.message.text + '成功');
         }
       })
 
     }
+    else if (event.message.type === 'sticker') {
+      //https://stickershop.line-scdn.net/stickershop/v1/sticker/428303664/ANDROID/sticker.png
+      catchUser(event.source.userId).then(ee => {
+        if (ee) {
+          let getObj = doObj(event, ee);
+          sendMessage({
+            ...getObj,
+            message: `https://stickershop.line-scdn.net/stickershop/v1/sticker/${event.message.stickerId}/ANDROID/sticker.png`,
+          });
+          event.reply('貼圖成功');
+        }
+      })
+    } else if (event.message.type === 'image') {
+
+      const downloadPath = path.join(__dirname, 'downloaded', `${event.message.id}.jpg`);
+      downloadContent(event.message.id, downloadPath)
+        .then((downloadPath) => {
+          return {
+            originalContentUrl: baseURL + '/downloaded/' + path.basename(downloadPath),
+          }
+          event.reply('下載圖片成功');
+        });
+    }
 
   })
 }
+
+const downloadContent=(messageId, downloadPath)=> {
+  return client.getMessageContent(messageId)
+    .then((stream) => new Promise((resolve, reject) => {
+      const writable = fs.createWriteStream(downloadPath);
+      stream.pipe(writable);
+      stream.on('end', () => resolve(downloadPath));
+      stream.on('error', reject);
+    }));
+}
+
 
 const catchUser = (id) => new Promise((resolve, reject) => {
   //從訊息只能撈出聊天訊息跟id，要另外撈出資料
@@ -94,3 +135,4 @@ exports = module.exports = {};
 exports.bot = bot;
 exports.botGet = getBot();
 exports.client = client;
+
