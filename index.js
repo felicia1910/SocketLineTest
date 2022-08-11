55b2c07d6f6dd9713c13511f22ba2cfaf82034b2
Felicia <flinder410010@gmail.com>
2022-08-11 15:03:24 +0800

    debug

--- index.js
+++ index.js
@@ -28,10 +28,7 @@ app.get('/link', function(req, res){
   let url=port==4000?'https://localhost:4000/callback':url+'/callback';
   res.render('Link/linkPage.html',{url: url});
 });
-// app.get("/link",(req,res)=>{
-//   let url=port==4000?'https://localhost:4000/callback':url+'/callback';
-//   res.sendFile(path.join(__dirname, '/Link/linkPage.html'),{url: url});
-// })
+
 // a http endpoint for trigger broadcast
 app.get("/callback",(req, res, next)=> {
   res.end("wait...")
